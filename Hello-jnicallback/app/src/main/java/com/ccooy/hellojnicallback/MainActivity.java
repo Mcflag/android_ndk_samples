56e4d9cb4e840dfe47048c6448f8399d0b8963e9
chen <301082528@qq.com>
2019-09-26 15:31:08 +0800

    update

---
+++ Hello-jnicallback/app/src/main/java/com/ccooy/hellojnicallback/MainActivity.java
@@ -0,0 +1,70 @@
+package com.ccooy.hellojnicallback;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.annotation.Keep;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    int hour = 0;
+    int minute = 0;
+    int second = 0;
+    TextView tickView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tickView = (TextView) findViewById(R.id.tickView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hour = minute = second = 0;
+        ((TextView) findViewById(R.id.hellojniMsg)).setText(stringFromJNI());
+        startTicks();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        StopTicks();
+    }
+
+    /*
+     * A function calling from JNI to update current timer
+     */
+    @Keep
+    private void updateTimer() {
+        ++second;
+        if (second >= 60) {
+            ++minute;
+            second -= 60;
+            if (minute >= 60) {
+                ++hour;
+                minute -= 60;
+            }
+        }
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String ticks = "" + MainActivity.this.hour + ":" +
+                        MainActivity.this.minute + ":" +
+                        MainActivity.this.second;
+                MainActivity.this.tickView.setText(ticks);
+            }
+        });
+    }
+
+    static {
+        System.loadLibrary("hello-jnicallback");
+    }
+
+    public native String stringFromJNI();
+
+    public native void startTicks();
+
+    public native void StopTicks();
+}
