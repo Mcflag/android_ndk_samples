ce70ca8f772fe4c6535a1bc42ecaecc1d77eb619
chen <301082528@qq.com>
2019-09-27 09:28:34 +0800

    update

--- Hello-jni/app/src/main/cpp/hello-jni.c
+++ Hello-jni/app/src/main/cpp/hello-jni.c
@@ -1,39 +1,43 @@
 #include <string.h>
 #include <jni.h>
+#include <inttypes.h>
+#include <pthread.h>
+#include <android/log.h>
+#include <assert.h>
 
 JNIEXPORT jstring JNICALL
-Java_com_ccooy_hellojni_HelloJni_stringFromJNI(JNIEnv *env, jobject thiz) {
+Java_com_ccooy_hellojni_HelloJni_stringFromJNI( JNIEnv* env, jobject thiz )
+{
 #if defined(__arm__)
     #if defined(__ARM_ARCH_7A__)
-        #if defined(__ARM_NEON__)
-            #if defined(__ARM_PCS_VFP)
-                #define ABI "armeabi-v7a/NEON (hard-float)"
-            #else
-                #define ABI "armeabi-v7a/NEON"
-            #endif
-        #else
-            #if defined(__ARM_PCS_VFP)
-                #define ABI "armeabi-v7a (hard-float)"
-            #else
-                #define ABI "armeabi-v7a"
-            #endif
-        #endif
+    #if defined(__ARM_NEON__)
+      #if defined(__ARM_PCS_VFP)
+        #define ABI "armeabi-v7a/NEON (hard-float)"
+      #else
+        #define ABI "armeabi-v7a/NEON"
+      #endif
     #else
-        #define ABI "armeabi"
+      #if defined(__ARM_PCS_VFP)
+        #define ABI "armeabi-v7a (hard-float)"
+      #else
+        #define ABI "armeabi-v7a"
+      #endif
     #endif
+  #else
+   #define ABI "armeabi"
+  #endif
 #elif defined(__i386__)
-    #define ABI "x86"
+#define ABI "x86"
 #elif defined(__x86_64__)
-    #define ABI "x86_64"
+#define ABI "x86_64"
 #elif defined(__mips64)  /* mips64el-* toolchain defines __mips__ too */
-    #define ABI "mips64"
+#define ABI "mips64"
 #elif defined(__mips__)
-    #define ABI "mips"
+#define ABI "mips"
 #elif defined(__aarch64__)
-    #define ABI "arm64-v8a"
+#define ABI "arm64-v8a"
 #else
-    #define ABI "unknown"
+#define ABI "unknown"
 #endif
-
     return (*env)->NewStringUTF(env, "Hello from JNI !  Compiled with ABI " ABI ".");
 }
---
+++ Hello-libs/app/src/main/cpp/hello-libs.cpp
@@ -0,0 +1,33 @@
+#include <cstring>
+#include <jni.h>
+#include <cinttypes>
+#include <android/log.h>
+#include <gmath.h>
+#include <gperf.h>
+#include <string>
+
+#define LOGI(...) \
+  ((void)__android_log_print(ANDROID_LOG_INFO, "hello-libs::", __VA_ARGS__))
+
+/* This is a trivial JNI example where we use a native method
+ * to return a new VM String. See the corresponding Java source
+ * file located at:
+ *
+ *   app/src/main/java/com/example/hellolibs/MainActivity.java
+ */
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_ccooy_hellolibs_MainActivity_stringFromJNI(JNIEnv *env, jobject thiz) {
+    // Just for simplicity, we do this right away; correct way would do it in
+    // another thread...
+    auto ticks = GetTicks();
+
+    for (auto exp = 0; exp < 32; ++exp) {
+        volatile unsigned val = gpower(exp);
+        (void) val;  // to silence compiler warning
+    }
+    ticks = GetTicks() - ticks;
+
+    LOGI("calculation time: %" PRIu64, ticks);
+
+    return env->NewStringUTF("Hello from JNI LIBS!");
+}
